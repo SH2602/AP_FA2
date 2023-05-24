1d3dbbeb4d0afb0ae167e7bfaa93a1cca6194196
SH2602 <sanne.hulleman@hu.nl>
2023-05-24 15:39:21 +0200

    made class employee heather fiile

---
+++ warehouse/src/include/employee.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+
+class Employee{
+    private:
+        std::string name;
+        bool busy;
+        bool forkliftCertificate;
+
+    public:
+       Employee(std::string name, bool forkliftCertificate);
+       std::string getName();
+       bool getBusy();
+       void setBusy(bool busy);
+       bool getForkliftCertificate();
+       void setForkliftCertificate(bool forkliftCertificate);
+};
