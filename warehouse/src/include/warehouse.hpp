095a3ac62db46d0e4912a15f67c2e4f42d5c3511
SH2602 <sanne.hulleman@hu.nl>
2023-05-24 15:57:14 +0200

    made class Warehouse in warehouse heather file

---
+++ warehouse/src/include/warehouse.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include<iostream>
+#include<vector>
+
+#include"include/employee.hpp"
+#include"include/shelf.hpp"
+
+
+class Warehouse{
+    public:
+        std::vector<Employee> Employees;
+        std::vector<Shelf> Shelves;
+
+        Warehouse();
+        void addEmployee(Employee employee);
+        void addShelf(Shelf shelf);
+        bool rearrangeShelf(Shelf& shelf);
+        bool pickItems(std::string itemName, int itemCount);
+};
