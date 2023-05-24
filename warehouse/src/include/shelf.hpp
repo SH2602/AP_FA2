c91686b89c4843d3901ee67c9f322087cc3c2521
SH2602 <sanne.hulleman@hu.nl>
2023-05-24 16:12:24 +0200

    added icontainer functions

--- warehouse/src/include/shelf.hpp
+++ warehouse/src/include/shelf.hpp
@@ -3,9 +3,9 @@
 #include<iostream>
 #include<array>
 #include "include/pallet.hpp"
+#include"include/icontainer.hpp"
 
-
-class Shelf{
+class Shelf:public IContainer{
     public:
         std::array<Pallet,4> pallets;
 
@@ -13,4 +13,6 @@ class Shelf{
         std::array<bool, 4> getSlotStatus();
         bool removePallet(int slot);
         bool insertPallet(int slot, Pallet pallet);
+        bool isEmpty()override;
+        bool isFull()override;
 };
